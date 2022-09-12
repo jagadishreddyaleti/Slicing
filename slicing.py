15d7f1174ea58ce5b7b189fb445b1cb49c0df7cf
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-09-12 12:10:00 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,36 @@
+""" 12th sep 2022"""
+'''slicing with single colon '''
+# a='python'
+# # print(a[0])
+# print(a[0:4])
+# print(a[0:])
+# print(a[1:])
+# print(a[0:3])
+# print(a[2:3])
+# print(a[3:2])
+
+# a='python'
+# print(a[-1:])
+# print(a[-2:])
+# print(a[-6:])
+# print(a[-1:-2])
+# print(a[-5:-2])
+
+''' slicing with double colon'''
+
+# a='python'
+# print(a[0::1])
+# print(a[::])
+# print(a[0:2:1])
+# print(a[2:7:2])
+# print(a[1:7:2])
+# print(a[0:7:2])
+
+# a='python'
+# print(a[::-1])
+# print(a[::-2])
+# print(a[::-3])
+
+''' palindrome cheak '''
+a='madam'
+print(a[::-1])
